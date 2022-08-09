9efab2a5143bb63b92fbc2225b186cd5da89ba70
FELIXYU-THINKPA\felix <felixyu.think@gmail.com>
2022-08-09 17:24:24 +0800

    商品功能 - 設定返回的時間格式

--- src/main/java/com/felixyu/springbootmall/controller/ProductController.java
+++ src/main/java/com/felixyu/springbootmall/controller/ProductController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 @RestController
 public class ProductController {
 
