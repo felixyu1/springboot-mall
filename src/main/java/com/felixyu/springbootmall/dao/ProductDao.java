23026bb9202951cf5e0a764a81b9f388d3449bcc
FELIXYU-THINKPA\felix <felixyu.think@gmail.com>
2022-08-10 23:28:57 +0800

    商品功能 - 實作查詢商品列表 基礎實作

--- src/main/java/com/felixyu/springbootmall/dao/ProductDao.java
+++ src/main/java/com/felixyu/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.felixyu.springbootmall.dao;
 import com.felixyu.springbootmall.dto.ProductRequest;
 import com.felixyu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProducyById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/felixyu/springbootmall/service/ProductService.java
+++ src/main/java/com/felixyu/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.felixyu.springbootmall.service;
 import com.felixyu.springbootmall.dto.ProductRequest;
 import com.felixyu.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
