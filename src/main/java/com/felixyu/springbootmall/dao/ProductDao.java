1f805bb56f3a8689add8e22af031995ca7413c93
FELIXYU-THINKPA\felix <felixyu.think@gmail.com>
2022-08-10 16:19:50 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/felixyu/springbootmall/dao/ProductDao.java
+++ src/main/java/com/felixyu/springbootmall/dao/ProductDao.java
@@ -8,4 +8,6 @@ public interface ProductDao {
     Product getProducyById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
+
+    void updateProduct(Integer productId, ProductRequest productRequest);
 }
