53e4cfe86eff3d2a91ceb419c6c1ac6eb3f1628d
FELIXYU-THINKPA\felix <felixyu.think@gmail.com>
2022-08-10 17:47:14 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/felixyu/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/felixyu/springbootmall/dao/impl/ProductDaoImpl.java
@@ -100,4 +100,15 @@ public class ProductDaoImpl implements ProductDao {
 
         namedParameterJdbcTemplate.update(sql, map);
     }
+
+    @Override
+    public void deleteProductById(Integer productId) {
+
+        String sql = " DELETE FROM product WHERE product_id = :productId ";
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+
+        namedParameterJdbcTemplate.update(sql, map);
+    }
 }
