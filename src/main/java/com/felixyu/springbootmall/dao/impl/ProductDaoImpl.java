a94ac24741ac3b26c5bb0a023d8471dbf80d7623
FELIXYU-THINKPA\felix <felixyu.think@gmail.com>
2022-08-13 11:39:19 +0800

    商品功能 - 提煉共用程式

--- src/main/java/com/felixyu/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/felixyu/springbootmall/dao/impl/ProductDaoImpl.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class ProductDaoImpl implements ProductDao {
@@ -34,15 +35,7 @@ public class ProductDaoImpl implements ProductDao {
 
         HashMap<String, Object> map = new HashMap<>();
 
-        if(productQueryParams.getCategory() != null){
-            sql = sql + " AND category = :category";
-            map.put("category", productQueryParams.getCategory().name());
-        }
-
-        if(productQueryParams.getSearch() != null){
-            sql = sql + " AND product_name like :search";
-            map.put("search", "%"+productQueryParams.getSearch()+"%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
 
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " "
                 + productQueryParams.getSort();
@@ -87,15 +80,7 @@ public class ProductDaoImpl implements ProductDao {
 
         HashMap<String, Object> map = new HashMap<>();
 
-        if(productQueryParams.getCategory() != null){
-            sql = sql + " AND category = :category";
-            map.put("category", productQueryParams.getCategory().name());
-        }
-
-        if(productQueryParams.getSearch() != null){
-            sql = sql + " AND product_name like :search";
-            map.put("search", "%"+productQueryParams.getSearch()+"%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
 
         Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
 
@@ -167,5 +152,20 @@ public class ProductDaoImpl implements ProductDao {
         map.put("productId", productId);
 
         namedParameterJdbcTemplate.update(sql, map);
+    }
+
+    private String addFilteringSql(String sql, Map<String, Object> map, ProductQueryParams productQueryParams){
+
+        if(productQueryParams.getCategory() != null){
+            sql = sql + " AND category = :category ";
+            map.put("category", productQueryParams.getCategory().name());
+        }
+
+        if(productQueryParams.getSearch() != null){
+            sql = sql + " AND product_name like :search ";
+            map.put("search", "%"+productQueryParams.getSearch()+"%");
+        }
+
+        return sql;
     }
 }
